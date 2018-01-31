ce46dd7d8471dfd6b193ea062ba1b6c22e3fb8a2
André Santos Meireles <andresmeireles@users.noreply.github.com>
2018-01-31 10:18:44 -0200

    Revert "Grab my commit"

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require __DIR__.'/../vendor/autoload.php';
 $http = new Client();
 
 $page = $http->request('GET', 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/Cnpjreva_Solicitacao.asp');
-$cnpj = '05020839000105';
 
-dump($page['headers'], $cnpj);
+echo "oi";
+
+dump($page['headers']);
