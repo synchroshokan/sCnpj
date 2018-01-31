252f6a44a75311a7e2584feca47760ade58bdbbb
Synchro Shokan <andre.e.legal@hotmail.com>
2018-01-31 15:31:58 +0000

    definition of package

--- public/index.php
+++ public/index.php
@@ -5,7 +5,27 @@ require __DIR__.'/../vendor/autoload.php';
 
 $http = new Client();
 
-$page = $http->request('GET', 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/Cnpjreva_Solicitacao.asp');
-$cnpj = '05020839000105';
+$request = $http->request('GET', 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/Cnpjreva_Solicitacao.asp');
+$headers = $request->getHeaders();
+$cookie = $headers['Set-Cookie'];
 
-dump($page['headers'], $cnpj);
+$image = $http->request('GET', 'http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/captcha/gerarCaptcha.asp');
+$x = $image->getBody()->getContents();
+
+echo base64_encode($x);
+
+file_put_contents(__DIR__.'/z.png', $x);
+
+dump($image->getBody()->getContents(), $cookie);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Captch</title>
+</head>
+<body>
+	<label>Imagem<input type="text" name="" value="">
+	<img src="z.png" alt="">
+	</label>
+</body>
+</html>
